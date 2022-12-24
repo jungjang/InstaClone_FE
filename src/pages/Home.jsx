10e2a67dc49654d0feaf77678b71c8af363cc146
정창원 <8500jcw@gmail.com>
2022-12-24 15:23:29 +0900

    :sparkles: 메인 뿌려줄 카드 1차완성 생성 페이지 css 1차완성 홈에 간단히 묶어봄

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import styled from "styled-components";
+import Header from "../components/Home/Header";
+import PostCard from "../components/Home/PostCard";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <StHome>
+        <Header />
+        <StPostCardList>
+          <PostCard />
+        </StPostCardList>
+      </StHome>
+    </>
+  );
 };
 
 export default Home;
+
+const StHome = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const StPostCardList = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  margin: 0 auto 0 10%;
+  /* align-items: center; */
+`;
